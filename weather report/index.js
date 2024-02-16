c3cdb58339757ef6bacb0d30a3d3408d8a4990e5
AL <112848500+DevyatovAlexy@users.noreply.github.com>
2024-02-16 18:17:57 +0500

    Update index.js

--- weather report/index.js
+++ weather report/index.js
@@ -4,7 +4,5 @@ const fetch = require('node-fetch')
 fetch("https://api.open-meteo.com/v1/forecast?latitude=57.09&longitude=65.32&current=temperature_2m,wind_speed_10m")
     .then(res => res.json())
     .then(json => {
-        console.log(json)
-
-
+        console.log(json.current.temperature_2m)
     })
