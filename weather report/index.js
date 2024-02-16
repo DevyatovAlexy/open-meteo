37afae061ce84234f79f23f11ec2a7dffd475db3
AL <112848500+DevyatovAlexy@users.noreply.github.com>
2024-02-16 19:43:23 +0500

    Delete weather report directory

--- weather report/index.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch')
-function snowMeteo(latitude,longitude) {
-    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m`)
-        .then(res => res.json(latitude,longitude))
-        .then(json => {
-            console.log(json.current.temperature_2m)
-        })
-}
-snowMeteo(57.09,65.32)
