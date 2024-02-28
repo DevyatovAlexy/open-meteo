0f66c823cb15472ee8054c257daf27b9a300370c
AL <112848500+DevyatovAlexy@users.noreply.github.com>
2024-02-28 19:48:44 +0500

    Meteo.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const WEATHER_API_BASE_URL = 'https://api.open-meteo.com/v1/forecast'
 const CITY_API_BASE_URL = 'https://geocode.maps.co/search'
 const API_KEY = '65d20f54dd6ed218876641rqo38531d'
 
-displayUrl('Тюмень')
+
 
 async function apiRequest(url, params) {
     try {
@@ -11,7 +11,7 @@ async function apiRequest(url, params) {
         let response = await fetch(url)
         let json = await response.json()
         if (response.status !== 200 && json.length <= 0) {
-            throw new Error('Wrong argument %argumentName%')
+            throw new Error('Request not successful')
         }
         return json
     } catch (error) {
@@ -19,32 +19,28 @@ async function apiRequest(url, params) {
     }
 }
 
-async function displayUrl(value,) {
-    const coordinats = await getCityCoordinates(value)
-    const temperatur = await getMeteo(coordinats.lat, coordinats.lon)
-    if (coordinats === undefined) {
+async function getTemperature(value) {
+    const coordinates = await getCityCoordinates(value)
+    const temperature = await getMeteo(coordinates[0].lat, coordinates[0].lon)
+    if (coordinates === undefined) {
         throw new Error('Wrong argument %Сity coordinates are not defined%')
     }
-    if (temperatur === undefined) {
+    if (temperature === undefined) {
         throw new Error('Wrong argument %Тo temperature found%')
-    } else console.log(temperatur.current.temperature_2m)
+    } else return temperature.current
 }
 
 //Получает название название города и возвращает координаты
 async function getCityCoordinates(value) {
     if (value == null || value.trim().length === 0 || !isNaN(value)) {
-        throw new Error('Wrong argument %argumentName%')
+        throw new Error('Wrong argument %cityName%')
     }
     const url = new URL(CITY_API_BASE_URL)
     const params = {
         q: value, api_key: API_KEY,
     }
-    const resultApi = await apiRequest(url, params)
-    // Возвращает первый объект который находит и передает
-    return resultApi.find(item => {
-        return item
-    })
-}
+    return await apiRequest(url, params)
+   }
 
 //Получает (latitude, longitude) координаты и возвращает температуру (json.current.temperature_2m)
 async function getMeteo(latitude, longitude) {
@@ -57,4 +53,4 @@ async function getMeteo(latitude, longitude) {
     }
     return await apiRequest(url, params)
 }
-
+module.exports = getTemperatu
